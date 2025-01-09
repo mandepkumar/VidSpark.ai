99063f265675c73ef1265f0d852f2e3ca5365558
Mandeep Kumar <mandeepk7856@gmail.com>
2025-01-09 22:24:20 +0530

    code setup

---
+++ components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-50 py-12">
+      <div className="container mx-auto px-4 text-center text-gray-600\">
+        <p>
+          Made with ❣️ by <span className="font-bold text-[#f92f60]">MK</span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className="py-12 text-center">Header</header>;
+};
+
+export default Header;
